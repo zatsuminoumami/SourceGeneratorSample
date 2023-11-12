18af4013348f4b5d351aada398ed1f86b2e13869
zatsu <fuwafumikanofu@gmail.com>
2023-11-13 05:30:58 +0900

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿using SourceGeneratorSample;
+
+var mc = new MyClass() { Hoge = 10, Bar = "tako" };
+Console.WriteLine(mc); // 出力 : Hoge:10, Bar:tako
+
+[GenerateToString]
+public partial class MyClass
+{
+    public int Hoge { get; set; }
+    public string? Bar { get; set; }
+}
